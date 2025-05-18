76500524d80f082c409d24bb5db743c78c0353a4
TigoreiT <tigore.mob@gmail.com>
2025-05-18 14:22:55 +0300

    Styles

--- components/product/index.js
+++ components/product/index.js
@@ -1,30 +1,31 @@
 export class ProductComponent {
-    constructor(parent) {
-        this.parent = parent
-    }
+	constructor(parent) {
+		this.parent = parent
+	}
 
-    getHTML(data) {
-        return (
-            `
-                <div class="card mb-3" style="width: 540px;">
+	getHTML(data) {
+		return `
+            <div class="product-page">
+                <div class="card mb-3" style="max-width: 800px;">
                     <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="${data.src}" class="img-fluid" alt="картинка">
+                        <div class="col-md-6">
+                            <img src="${data.src}" class="img-fluid rounded-start" alt="${data.title}">
                         </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">${data.title}</h5>
-                                <p class="card-text">${data.text}</p>
-                            </div>
+                    <div class="col-md-6">
+                        <div class="card-body">
+                            <h2 class="card-title">${data.title}</h2>
+                            <p class="card-text">${data.text}</p>
+                            <p class="text-muted">Акция действует до 31.12.2023</p>
                         </div>
                     </div>
                 </div>
-            `
-        )
-    }
+            </div>
+        </div>
+    `
+	}
 
-    render(data) {
-        const html = this.getHTML(data)
-        this.parent.insertAdjacentHTML('beforeend', html)
-    }
+	render(data) {
+		const html = this.getHTML(data)
+		this.parent.insertAdjacentHTML('beforeend', html)
+	}
 }
